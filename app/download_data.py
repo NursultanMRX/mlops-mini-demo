4f42eda11bae04bfa37b1649d6b37bc5c9c7e76e
MRX <121639994+NursultanMRX@users.noreply.github.com>
2026-03-19 11:26:29 +0500

    Create download_data.py

---
+++ app/download_data.py
@@ -0,0 +1,21 @@
+# download_data.py
+import os
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+
+# GitHub Actions secret
+gdrive_json = os.environ["GDRIVE_KEY"]
+with open("service_account.json", "w") as f:
+    f.write(gdrive_json)
+
+gauth = GoogleAuth()
+gauth.ServiceAuthSettings = {"client_json_file": "service_account.json"}
+gauth.ServiceAuth()
+drive = GoogleDrive(gauth)
+
+# Google Drive file ID
+file_id = "GOOGLE_DRIVE_FILE_ID"  # Adult Income CSV file ID
+downloaded = drive.CreateFile({"id": file_id})
+os.makedirs("data", exist_ok=True)
+downloaded.GetContentFile("data/adult11.csv")
+print("Adult Income CSV downloaded successfully!")
