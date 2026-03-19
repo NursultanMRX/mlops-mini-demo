6648d60c8c1ea78f32bc1f5ba0881ce9d7390437
MRX <121639994+NursultanMRX@users.noreply.github.com>
2026-03-19 10:01:39 +0500

    Add training script for Random Forest model

---
+++ app/train.py
@@ -0,0 +1,13 @@
+# app/train.py
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+def train():
+    data = load_iris()
+    X, y = data.data, data.target
+
+    model = RandomForestClassifier()
+    model.fit(X, y)
+
+    joblib.dump(model, "app/model.pkl")
