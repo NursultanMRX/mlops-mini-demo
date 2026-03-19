0a6684b9d4c63e9afc932eb22fc7a4d90f3ad5c0
MRX <121639994+NursultanMRX@users.noreply.github.com>
2026-03-19 12:00:23 +0500

    Update download_data.py

--- app/download_data.py
+++ app/download_data.py
@@ -4,22 +4,22 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
 
-# 1. GitHub Secret'dan JSON'ni o'qib faylga yozish
+# 1. GitHub Secret
 gdrive_json = os.environ["GDRIVE_KEY"]
 with open("service_account.json", "w") as f:
     f.write(gdrive_json)
 
-# 2. Service Account bilan to'g'ridan-to'g'ri autentifikatsiya
+# 2. Service Account bilan ulanish
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 creds = service_account.Credentials.from_service_account_file(
     'service_account.json', scopes=SCOPES
 )
 
-# 3. Drive API'ni ulash
+# 3. Drive API
 service = build('drive', 'v3', credentials=creds)
 
-# 4. Faylni yuklab olish
-file_id = "1lLaxBSyKS-eZHclBWeY24ollDPXM1PdB" # Faylingizning ID sini yozishni unutmang!
+# 4. Sizning maxsus File ID'ingiz
+file_id = "1lLaxBSyKS-eZHclBWeY24ollDPXM1PdB"
 request = service.files().get_media(fileId=file_id)
 
 os.makedirs("data", exist_ok=True)
